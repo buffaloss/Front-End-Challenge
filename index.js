44a51e959e0d6c16af267054d76d9e64e8b79080
Beatrice <beatrice70712@gmail.com>
2023-03-12 11:36:09 +0200

    3rd task

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const buttons = document.querySelectorAll(".keys");
 
-for (let i = 0 ; i < buttons.length; i++) {
-  buttons[i].addEventListener("mousedown", function() {
-    const button = buttons[i];
-    button.classList.add("blue");
-  });
-  
-  buttons[i].addEventListener("mouseup", function() {
-    const button = buttons[i];
-    button.classList.remove("blue");
-    button.classList.add("green");
-  });
-  
-  buttons[i].addEventListener("mouseleave", function() {
-    const button = buttons[i];
-    button.classList.remove("blue");
-  });
+for (let i=0; i<buttons.length; i++){
+    buttons[i].addEventListener("click",function(){
+        const button = buttons[i];
+        button.classList.add("blue");
+        setTimeout(() => {
+            button.classList.remove("blue");
+            button.classList.add("green");
+          }, "3000");
+    })
 }
